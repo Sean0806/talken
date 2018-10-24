4fac468a5248dd74ef383787b0a505e7c51e2b10
shencong <jestersean@163.com>
2018-10-24 13:52:36 +0800

    注释

---
+++ src/main/java/com/talken/talken/TalkenApplication.java
@@ -0,0 +1,12 @@
+package com.talken.talken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalkenApplication.class, args);
+    }
+}
